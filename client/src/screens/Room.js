e23e5e2bb4c1d95913c28e2129f3f59b92747923
Dheeraj <i.dheerajdileep@gmail.com>
2023-05-08 01:20:46 +0530

    connected two people

--- client/src/screens/Room.js
+++ client/src/screens/Room.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useCallback,useState}from 'react'
 import { useSocket } from '../context/SocketProvider'
 import Reactplayer  from 'react-player'
+import peer from '../service/Peer'
 
 function Room() {
     const socket = useSocket()
@@ -11,32 +12,66 @@ function Room() {
       console.log(`Email ${email} joined`)
       setRemoteSocketId(id)
     },[])
+    const handleIncommingCall = useCallback(
+      async ({ from, offer }) => {
+        setRemoteSocketId(from);
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
+        setMyStream(stream);
+        console.log(`Incoming Call`, from, offer);
+        const ans = await peer.getAnswer(offer);
+        socket.emit("call:accepted", { to: from, ans });
+      },
+      [socket]
+    );
+    const handleCallAccepted = useCallback(
+      async ({ from, ans }) => {
+        try {
+          peer.setLocalDescription(ans);
+          console.log("Call Accepted!");
+        } catch (error) {
+          console.error("Error setting remote description: ", error);
+        }
+      },[]
+    );
     useEffect(()=>{
         socket.on('user:joined',handleUserJoined)
+        socket.on('incomming:call',handleIncommingCall)
+        socket.on('call:accepted',handleCallAccepted)
         return()=>{
           socket.off('user:joined',handleUserJoined)
+          socket.off('incomming:call',handleIncommingCall)
+          socket.off('call:accepted',handleCallAccepted)
         }
-    },[socket,handleUserJoined])
+    },[socket,handleUserJoined,handleIncommingCall,handleCallAccepted])
     const handleCallUser = useCallback(async()=>{
       const stream = await navigator.mediaDevices.getUserMedia({
         audio:true,
         video:true
       })
+      const offer = await peer.getOffer()
+      socket.emit('user:call',{to:remoteSocketId, offer})
       setMyStream(stream)
-    },[])
+    },[remoteSocketId,socket])
+
   return (
     <div>
       <h1>Room page</h1>
       <h3>{remoteSocketId ? 'Connected' : 'No one in the room'}</h3>
       {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
-      {myStream && 
+      {myStream && (
+      <>
+      <h1>My stream</h1>
       <Reactplayer
        playing
        muted
        url={myStream}
-       height='100px'
-       width='200px'/>}
-    </div>
-  )
-}
+       height='300px'
+       width='500px'/>
+    </>)}
+   </div>
+    )}
+
 export default Room
